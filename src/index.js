a4204aac6919f14ebcbd956cdf9c8a7520ac6024
Lucas Ramos <lucasrcdias@gmail.com>
2018-01-13 15:02:09 -0200

    Add initial routing for home and signup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import SignUp from './components/signup';
+import Home from './components/home';
 
 ReactDOM.render(
-  <div><h1>Customer Management</h1></div>,
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </div>
+  </Router>,
   document.getElementById('root')
 );
