fa4af2ca71b2cf6f0512aec8d55a8a1ea7dded35
Lucas Ramos <lucasrcdias@gmail.com>
2018-01-21 20:21:44 -0200

    Create form group component

--- src/components/presentationals/forms/index.js
+++ src/components/presentationals/forms/index.js
@@ -3,9 +3,11 @@ import hint from './hint';
 import required from './required';
 import label from './label';
 import input from './input';
+import formGroup from './form-group';
 
 export const Validation = validation;
 export const Hint = hint;
 export const Required = required;
 export const Label = label;
 export const Input = input;
+export const FormGroup = formGroup;
