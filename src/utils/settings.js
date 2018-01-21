1daacd76b02e7e27fc7482440bf510afba953419
Lucas Ramos <lucasrcdias@gmail.com>
2018-01-21 18:23:02 -0200

    Move settings file and set apiUrl key camelCased

---
+++ src/utils/settings.js
@@ -0,0 +1,13 @@
+const configuration = {
+  "production": {
+    "apiUrl": "http://customermgmt.com/api"
+  },
+  "staging": {
+    "apiUrl": "http://staging.customermgmt.com/api"
+  },
+  "development": {
+    "apiUrl": "http://localhost:3000/api"
+  }
+}
+
+export default configuration;
