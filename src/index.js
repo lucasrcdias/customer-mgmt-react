024f12931fdb268d69ed2cd0639796f683eac0f6
Lucas Ramos <lucasrcdias@gmail.com>
2018-01-21 20:24:07 -0200

    Configure history and export store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router,
+  Router,
   Route,
   Switch
 } from 'react-router-dom';
+import history from './history';
 
 // Redux
 import { createStore } from 'redux';
@@ -15,20 +16,20 @@ import reducers from './reducers';
 import Routes from './utils/routes';
 
 // Pages
-import SignIn from './pages/signin';
-import SignUp from './pages/signup';
+import signIn from './components/signin';
+import signUp from './components/signup';
 
 import './index.css';
 
-const store = createStore(reducers);
+export const store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       <div>
         <Switch>
-          <Route exact path={Routes.signin} component={SignIn} />
-          <Route exact path={Routes.signup} component={SignUp} />
+          <Route exact path={Routes.signin} component={signIn} />
+          <Route exact path={Routes.signup} component={signUp} />
         </Switch>
       </div>
     </Router>
