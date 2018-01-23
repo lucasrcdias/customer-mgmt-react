cd15724b420b239aee8d1cddae27df126a250f2a
Lucas Ramos <lucasrcdias@gmail.com>
2018-01-22 22:00:58 -0200

    Add signout to case and fix assign

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,11 +1,12 @@
-import { SIGN_UP, SIGN_IN } from './../constants';
+import { SIGN_UP, SIGN_IN, SIGN_OUT } from './../constants';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case SIGN_UP:
     case SIGN_IN:
+    case SIGN_OUT:
       const user = action.payload;
-      return Object.assign({}, state, { user });
+      return Object.assign({}, state, user);
     default:
       return state;
   }
